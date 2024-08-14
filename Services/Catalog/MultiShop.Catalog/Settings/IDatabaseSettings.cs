333cfc33d60f6ca991f59e4b65047eb65a084c86
Rabia Baş <bassrabiaa@gmail.com>
2024-08-14 14:44:21 +0300

    Created Settings (IDatabaseSettings interface and DatabaseSettings class)

---
+++ Services/Catalog/MultiShop.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Catalog.Settings
+{
+    public class IDatabaseSettings
+    {
+        // Interface si olan şeyin mutlaka class ı da olmalı
+        // CRUD Create (Oluştur), Read (Oku), Update (Güncelle) ve Delete (Sil) , işlemleri için gerekli
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string ProductDetailCollectionName { get; set; }
+        public string ProductImageCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
