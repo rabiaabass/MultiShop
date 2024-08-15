cfa2f707b7278d90899d8f382ce9c7c906f33bd5
Rabia Baş <bassrabiaa@gmail.com>
2024-08-15 15:53:23 +0300

    Created ProductDtos

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDtos
+{
+    public class ResultProductDto
+    {
+        public string ProductId { get; set; } // nullable geçilebilir isteseydik string? yapacaktık
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDtos/UpdateProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDtos
+{
+    public class UpdateProductDto
+    {
+        public string ProductId { get; set; } // nullable geçilebilir isteseydik string? yapacaktık
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryId { get; set; }
+
+        // silme işlemleri için Dto ya gerek yok. Çünkü silme işlemi geriye bir şey getirmeyecek. Parametre olarak sadece id alacak.
+    }
+}
