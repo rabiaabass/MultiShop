05fccdff31464f66ec9dd3e4734d00b28b5beaa1
Rabia Baş <bassrabiaa@gmail.com>
2024-08-24 19:23:34 +0300

    Created ProductDetailService and ProductImageService

---
+++ Services/Catalog/MultiShop.Catalog/Controller/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Catalog.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
